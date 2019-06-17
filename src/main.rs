7212a2f947f878425f500eac3c2e482d2763854b
Belousow Makc <lib.bmw@gmail.com>
2019-06-17 19:05:43 +0300

    Try to convert Proxy::Result into ResponseFuture.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+#[macro_use]
+extern crate actix_derive;
 use actix::prelude::*;
 use actix_rt::spawn;
-use actix::dev::{MessageResponse, ResponseChannel};
+use actix::dev::MessageResponse;
 
 
 #[derive(Debug)]
@@ -10,11 +12,11 @@ struct Pong(i32);
 
 
 impl Message for Ping {
-    type Result = Result<i32, String>;
+    type Result = Result<i32, ServiceError>;
 }
 
 impl Message for Pong {
-    type Result = Result<i32, String>;
+    type Result = Result<i32, ServiceError>;
 }
 
 struct Upstream;
@@ -49,21 +51,84 @@ impl Actor for Proxy {
     type Context = Context<Self>;
 }
 
+trait Cacheable {
+}
 
+impl Cacheable for Ping {
+
+}
+
+#[derive(Debug, Message)]
+enum CachedMessages {
+    #[derive(Message)]
+    Ping(i32),
+    #[derive(Message)]
+    Pong(i32),
+}
+
+struct CacheMessage(pub CachedMessages);
+
+impl Message for CacheMessage {
+    type Result = Result<i32, ServiceError>;
+}
+
+
+// impl Handler<CacheMessage> for Proxy
+
+#[derive(Debug)]
+enum ServiceError {
+    Common
+}
+
+impl From<MailboxError> for ServiceError {
+    fn from(error: MailboxError) -> Self {
+        ServiceError::Common
+    }
+}
+
+impl From<String> for ServiceError {
+    fn from(error: MailboxError) -> Self {
+        ServiceError::Common
+    }
+}
+
+
+use futures::future::IntoFuture;
 impl<T> Handler<T> for Proxy 
 where
     T: Message + Send + 'static,
-    <T as Message>::Result: MessageResponse<Upstream, T> + Send,
-    <T as Message>::Result: MessageResponse<Proxy, T>,
-    Upstream: Handler<T>
+    <T as Message>::Result: IntoFuture + Send,
+    Upstream: Handler<T>,
+    <<T as Message>::Result as IntoFuture>::Error: From<MailboxError>,
 {
-    type Result = <T as Message>::Result;
+    type Result = ResponseFuture<<<<T as Message>::Result as IntoFuture>::Future as Future>::Item, <<<T as Message>::Result as IntoFuture>::Future as Future>::Error>;
 
     fn handle(&mut self, msg: T, _: &mut Context<Self>) -> Self::Result {
-        self.addr.send(msg)
-//        Err("Error".to_string())
+        let res = self
+            .addr
+            .send(msg)
+            .map_err(|e| e.into())
+            .and_then(|res| res);
+        Box::new(res)
     }
 }
+
+
+// impl<T> Handler<T> for Proxy 
+// where
+    // T: Message + Send + 'static,
+    // // <T as Message>::Result: MessageResponse<Upstream, T> + Send,
+    // <T as Message>::Result: MessageResponse<Proxy, T>,
+    // Upstream: Handler<T>,
+    // // <T as Message>::Result: std::convert::From<Request<Upstream, T>>,
+// {
+    // type Result = <T as Message>::Result;
+
+    // fn handle(&mut self, msg: T, _: &mut Context<Self>) -> Self::Result {
+        // self.addr.send(msg).unwrap()
+// //        Err("Error".to_string())
+    // }
+// }
 
 
 fn main() -> std::io::Result<()> {
